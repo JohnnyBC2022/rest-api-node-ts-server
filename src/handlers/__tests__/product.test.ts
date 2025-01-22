c156b88022c9239773c4948412f7b53499ad8331
Johnny <jonathanbc2012@gmail.com>
2025-01-22 11:42:26 +0100

    test: should display validation errors added

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -4,6 +4,19 @@ import server from '../../server'
 jest.setTimeout(10000) // añadimos un tiempo para que pueda ejecutar la prueba
 
 describe('/POST /api/v1/products', () => {
+    it('should display validation errors', async()=>{
+        const response = await request(server).post('/api/v1/products')
+        .send({})
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(4)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(2)
+    })
+
+
     it('should create a new product', async () => {
         const response = await request(server).post('/api/v1/products')
         .send({
