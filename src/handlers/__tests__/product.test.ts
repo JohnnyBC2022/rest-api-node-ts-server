e9cd292afe78280a5fcf816e68c2b13b3d0b07cd
Johnny <jonathanbc2012@gmail.com>
2025-01-22 11:49:22 +0100

    test: validate that the price is a number and greater than 0

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -16,6 +16,36 @@ describe('/POST /api/v1/products', () => {
         expect(response.body.errors).not.toHaveLength(2)
     })
 
+    it('should validate that the price is greater than 0', async()=>{
+        const response = await request(server).post('/api/v1/products')
+        .send({
+            name: 'Monitor',
+            price: 0
+        })
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(2)
+    })
+
+    it('should validate that the price is a number and greater than 0', async()=>{
+        const response = await request(server).post('/api/v1/products')
+        .send({
+            name: 'Monitor',
+            price: "Hola"
+        })
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(2)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(4)
+    })
+
 
     it('should create a new product', async () => {
         const response = await request(server).post('/api/v1/products')
