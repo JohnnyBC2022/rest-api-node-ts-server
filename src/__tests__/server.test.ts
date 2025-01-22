e45add75e01e1166e8c2023cb35d8a20bb38c3fb
Johnny <jonathanbc2012@gmail.com>
2025-01-22 20:40:38 +0100

    test: add new test in server to simulate database connection error

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,8 +1,9 @@
 import request from 'supertest'
-import server from '../server'
+import server, { connectDB } from '../server'
+import db from '../config/db'
 
-describe('GET /api',()=>{
-    it('should send back a json response', async()=>{
+describe('GET /api', () => {
+    it('should send back a json response', async () => {
         const res = await request(server).get('/api')
 
         expect(res.status).toBe(200)
@@ -11,5 +12,20 @@ describe('GET /api',()=>{
 
         expect(res.status).not.toBe(404)
         expect(res.body.msg).not.toBe('desde api')
+    })
+})
+
+// Con este test lo que hacemos es utilizar un mock para que en la conexión entre en el error del try catch, para poder cubrir la cobertura de esas líneas de código sin modificar el código original
+jest.mock('../config/db')
+
+describe('connectDB', () => {
+    it('should handle database connection error', async () => {
+        jest.spyOn(db, 'authenticate')
+            .mockRejectedValueOnce(new Error('Hubo un error al conectar a la base de datos'))
+        const consoleSpy = jest.spyOn(console, 'log')
+
+        await connectDB()
+
+        expect(consoleSpy).toHaveBeenCalledWith(expect.stringContaining('Hubo un error al conectar a la base de datos'))
     })
 })
